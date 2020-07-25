450df1f4c74357d1c9fe6dbf236d00199e6b7918
Mathias <mathiaswi177@gmail.com>
2020-07-24 21:10:21 -0700

    Updates

---
+++ precache-manifest.23c587cf11ab7015c9c026d81ea852bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f96c830ea31b2d7205b5e89be18f65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8717a03e8514de153dfd",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c89abd74f673716726f9",
+    "url": "/static/js/2.9450cc92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9450cc92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8717a03e8514de153dfd",
+    "url": "/static/js/main.dea6b247.chunk.js"
+  },
+  {
+    "revision": "b7979adc1557b5fb95df",
+    "url": "/static/js/runtime-main.665575f7.js"
+  },
+  {
+    "revision": "2880e40fbe7e71ba22e44c0760d06458",
+    "url": "/static/media/double-bubble.2880e40f.png"
+  }
+]);
